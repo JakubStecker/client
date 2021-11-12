07df2cdecf0cc64c7c98b8ce0af764c8018c71da
Jakub Štecker <jakub.stecker@seznam.cz>
2021-11-12 17:16:54 +0100

    I don't  know

--- store/index.js
+++ store/index.js
@@ -12,28 +12,36 @@ export const mutations = {
             item = state.products[i].cards.find(product => product.id == x.id)
             if (item != undefined) break
         }     
-        let cartItem = state.inCart.find(item => item.id == x.id)
-        let compareArray = true
-        if (cartItem != undefined) {
-            item.selectedIngredients.sort()
-            cartItem.selectedIngredients.sort()
-            for (let j = 0; j < item.selectedIngredients.length; j++) {
-                if (item.selectedIngredients.length != cartItem.selectedIngredients.length || item.selectedIngredients[j] != cartItem.selectedIngredients[j]) {
-                    compareArray = false
-                    console.log(compareArray)
-                    break 
-                }  
-            }
-        }
-        if (cartItem != null && item.id == cartItem.id && item.size == cartItem.size && compareArray) {
-            cartItem.quantity += x.quantity
-            console.log("It is true")
-            console.log(cartItem.size)
-        }
-        else {
-            state.inCart.push(item)
-            console.log("It is false")
-        }
+        // let cartItem = state.inCart.find(product => product.id == x.id)
+        state.inCart.push(item)
+        console.log(state.inCart)
+        // let compareArray = true
+        // if (cartItem != undefined) {
+        //     console.log("cartItem " + cartItem.size);
+        //     item.selectedIngredients.sort()
+        //     cartItem.selectedIngredients.sort()
+        //     console.log(item.selectedIngredients);
+        //     console.log(cartItem.selectedIngredients);
+        //     for (let j = 0; j < item.selectedIngredients.length; j++) {
+        //         if (item.selectedIngredients.length != cartItem.selectedIngredients.length || item.selectedIngredients[j] != cartItem.selectedIngredients[j]) {
+        //             compareArray = false
+        //             console.log(compareArray)
+        //             break 
+        //         }  
+        //     }
+        //     console.log("cartItem " + cartItem.size);
+        //     console.log(item.size);
+        //     console.log(cartItem.size)
+        // }
+        
+        // if (cartItem != undefined && item.id == cartItem.id && item.size == cartItem.size && compareArray) {
+        //     cartItem.quantity += x.quantity
+        //     console.log("It is true")
+        // }
+        // else {
+        //     state.inCart.push(item)
+        //     console.log("It is false")
+        // }
     },
     sizeCheck(state, x) {
         for (let i = 0; i < state.products.length; i++) {
